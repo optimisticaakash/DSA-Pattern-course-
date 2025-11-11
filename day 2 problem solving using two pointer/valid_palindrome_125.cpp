6319f569fa11523011c3eb91abfd702681334aa4
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2025-11-11 09:47:16 +0530

    Create valid_palindrome_125.cpp

---
+++ day 2 problem solving using two pointer/valid_palindrome_125.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.size()-1;
+
+        while(i < j){
+            char left = s[i];
+            char right = s[j];
+
+            if(!isalpha(left) && !isdigit(left)){
+                i++;
+                continue;
+            }
+
+            if(!isalpha(right) && !isdigit(right)){
+                j--;
+                continue;
+            }
+
+
+            if(tolower(left) != tolower(right)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+        
+    }
+};
